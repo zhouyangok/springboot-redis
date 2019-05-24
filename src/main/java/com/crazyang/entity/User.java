bc56e6033ad6b25dad24a9311cc9e0a3c0c447e0
zhouyangok <zhouyangx99@163.com>
2019-05-24 21:14:07 +0800

    添加用户信息

---
+++ src/main/java/com/crazyang/entity/User.java
@@ -0,0 +1,61 @@
+package com.crazyang.entity;
+
+/**
+ * @ClassName User
+ * @Description: TODO
+ * @Author zhouyang
+ * @Date 2019/5/24 下午9:12.
+ */
+
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private String img;
+
+    private String email;
+
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
