3ffbcf0aabae1d407ef23de183f12c37a0e1158e
zhouyangok <zhouyangx99@163.com>
2019-05-26 22:50:22 +0800

    controller

---
+++ src/main/java/com/crazyang/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.crazyang.controller;
+
+/**
+ * @ClassName UserController
+ * @Description: TODO
+ * @Author zhouyang
+ * @Date 2019/5/26 下午10:49.
+ */
+
+public class UserController {
+}
