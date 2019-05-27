da6367a4ca655c5e081ae35436ba0ab7037c2d8a
zhouyangok <zhouyangx99@163.com>
2019-05-27 16:09:04 +0800

    添加redis

--- src/main/java/com/crazyang/controller/UserController.java
+++ src/main/java/com/crazyang/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.crazyang.controller;
 
+import com.crazyang.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @ClassName UserController
  * @Description: TODO
@@ -7,5 +15,27 @@ package com.crazyang.controller;
  * @Date 2019/5/26 下午10:49.
  */
 
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    @RequestMapping("/getUser/{name}")
+    @ResponseBody
+    public User getUser(@PathVariable String name){
+       User redisUser = (User) redisTemplate.opsForValue().get(name);
+       if(redisUser!=null){
+           return redisUser;
+       }
+        User user = new User();
+        user.setId(100L);
+        user.setUsername("helloRedis");
+        user.setAge(123);
+        redisTemplate.opsForValue().set(user.getUsername(),user);
+        return user;
+    }
+
 }
--- src/main/java/com/crazyang/entity/User.java
+++ src/main/java/com/crazyang/entity/User.java
@@ -1,5 +1,7 @@
 package com.crazyang.entity;
 
+import java.io.Serializable;
+
 /**
  * @ClassName User
  * @Description: TODO
@@ -7,55 +9,81 @@ package com.crazyang.entity;
  * @Date 2019/5/24 下午9:12.
  */
 
-public class User {
+public class User implements Serializable {
 
-    private int id;
+    private static final long serialVersionUID = 1222221L;
 
-    private String name;
+    private Long id;
 
-    private String img;
+    /**
+     * 用户名
+     */
+    private String username;
 
-    private String email;
+    /**
+     * 密码
+     */
+    private String password;
 
-    private String address;
+    /**
+     * 年龄
+     */
+    private Integer age;
 
-    public int getId() {
+    /**
+     * 性别 1=男 2=女 其他=保密
+     */
+    private Integer sex;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
     }
 }
